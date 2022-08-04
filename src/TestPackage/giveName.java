ae3a222da76bfbc7d7f80e7cc2b63abb60205f7b
Lev Shapovalov <Leff200045@Gmail.com>
2022-08-04 15:28:45 +0500

    add !

--- src/TestPackage/giveName.java
+++ src/TestPackage/giveName.java
@@ -2,6 +2,6 @@ package TestPackage;
 
 public class giveName {
     public static void main(String[] args) {
-        System.out.println("Name - Ilya");
+        System.out.println("Name - Ilya!");
     }
 }
